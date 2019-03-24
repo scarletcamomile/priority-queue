adaca6dfa8e57655bc3fcf3b47c72a8b6ac18098
scarletcamomile <scarletcamomile@mail.ru>
2019-03-24 21:41:23 +0300

    add solution to node.js and half-solution to max-heap.js

--- src/max-heap.js
+++ src/max-heap.js
@@ -1,49 +1,65 @@
-const Node = require('./node');
+const Node = require("./node");
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
+		this.size = 0;
 	}
 
 	push(data, priority) {
-		
+		const node = new Node(data, priority);
+		this.insertNode(node);
+		this.shiftNodeUp(node);
+		this.size++;
 	}
 
 	pop() {
-		
+		this.size--;
 	}
 
-	detachRoot() {
-		
-	}
+	detachRoot() {}
 
-	restoreRootFromLastInsertedNode(detached) {
-		
-	}
+	restoreRootFromLastInsertedNode(detached) {}
 
 	size() {
-		
+		return this.size;
 	}
 
 	isEmpty() {
-		
+		if (this.root) return false;
+		else return true;
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
+		this.size = 0;
 	}
 
 	insertNode(node) {
-		
+		if (this.isEmpty()) {
+			this.root = node;
+			this.parentNodes.push(node);
+		} else {
+			this.parentNodes[0].appendChild(node);
+			this.parentNodes.push(node);
+			if (this.parentNodes[0].right) this.parentNodes.shift();
+		}
 	}
 
 	shiftNodeUp(node) {
-		
+		if (node.parent && node.priority > node.parent.priority) {
+			node.swapWithParent();
+			let nodeIndex = this.parentNodes.indexOf(node);
+			let parentIndex = this.parentNodes.indexOf(node.parent);
+			if (nodeIndex >= 0) this.parentNodes[nodeIndex] = node.parent;
+			if (parentIndex >= 0) this.parentNodes[parentIndex] = node;
+			this.shiftNodeUp(node);
+		}
 	}
 
-	shiftNodeDown(node) {
-		
-	}
+	shiftNodeDown(node) {}
 }
 
 module.exports = MaxHeap;
--- src/node.js
+++ src/node.js
@@ -1,22 +1,81 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if (!this.left) {
+			this.left = node;
+			node.parent = this;
+		} else if (this.left && !this.right) {
+			this.right = node;
+			node.parent = this;
+		}
 	}
 
 	removeChild(node) {
-
+		if (this.left === node) {
+			this.left = null;
+			node.parent = null;
+		} else if (this.right === node) {
+			this.right = null;
+			node.parent = null;
+		} else {
+			throw new Error("passed node is not a child of this node");
+		}
 	}
 
 	remove() {
-
+		if (this.parent) {
+			this.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
-		
+		//does nothing if node does not have parent
+		if (this.parent) {
+			let parent = this.parent;
+			if (this.parent.parent) {
+				let root = this.parent.parent;
+				this.parent = root; //updates child.parent (this.parent)
+				//maintains correct state of parent.parent.left and parent.parent.right
+				if (root.left === parent) {
+					root.left = this;
+				} else if (root.right === parent) {
+					root.right = this;
+				}
+			}
+			parent.parent = this; //updates parent.parent
+			//updates parent.child.parent
+			if (parent.right === this && parent.left) {
+				parent.left.parent = this;
+			} else if (parent.left === this && parent.right) {
+				parent.right.parent = this;
+			}
+			//updates parents of children of node
+			if (this.left) {
+				this.left.parent = parent;
+			}
+			if (this.right) {
+				this.right.parent = parent;
+			}
+			//updates children of node and parent node
+			let leftChild = parent.left;
+			let rightChild = parent.right;
+			parent.left = this.left;
+			parent.right = this.right;
+			if (leftChild === this) {
+				this.right = rightChild;
+				this.left = parent;
+			} else if (rightChild === this) {
+				this.left = leftChild;
+				this.right = parent;
+			}
+		}
 	}
 }
 
